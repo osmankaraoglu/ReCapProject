7277011cbb0e452022220ea48c23b5b0f96f83bd
Jxpac <Jxpac@DESKTOP-LF2NCQP>
2022-03-16 14:21:19 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,57 @@
+﻿using Business.Concrate;
+using DataAccess.Concrate.InMemory;
+using Entities.Concrate;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Car car1 = new Car();
+            car1.Id = 9;
+            car1.BrandId = 6;
+            car1.ColorId = 3;
+            car1.FuelTypeId = 1;
+            car1.Name = "Porche Cayenne";
+            car1.ModelYear = 2016;
+            car1.DailyPrice = 440;
+            car1.Description = "Otomatik Vites";
+
+            Car car2 = new Car();
+            car2.Id = 3;
+            car2.BrandId = 3;
+            car2.ColorId = 2;
+            car2.FuelTypeId = 1;
+            car2.Name = "BMW M5";
+            car2.ModelYear = 2016;
+            car2.DailyPrice = 500;
+            car2.Description = "Manuel Vites";
+
+            
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Name);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(carManager.GetById(2).Name);
+            Console.WriteLine();
+
+            carManager.Add(car1);
+            carManager.Update(car2);
+            carManager.Delete(new Car { Id = 1 });
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Name);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ DataAccess/Concrate/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,73 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrate.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> 
+            {
+            new Car { Id = 1, BrandId = 1, ColorId = 2, FuelTypeId = 1, Name = "Ford Focus",
+                ModelYear = 2018, DailyPrice= 350, Description = "Otomatik Vites" },
+            new Car { Id = 2, BrandId = 2, ColorId = 3, FuelTypeId = 2, Name = "Fiat Linea",
+                ModelYear = 2015, DailyPrice= 250, Description = "Manuel Vites" },
+            new Car { Id = 3, BrandId = 3, ColorId = 4, FuelTypeId = 1, Name = "BMW M3",
+                ModelYear = 2012, DailyPrice= 320, Description = "Manuel Vites" },
+            new Car { Id = 4, BrandId = 2, ColorId = 1, FuelTypeId = 1, Name = "Fiat Egea",
+                ModelYear = 2020, DailyPrice= 280, Description = "Manuel Vites" },
+            new Car { Id = 5, BrandId = 4, ColorId = 3, FuelTypeId = 3, Name = "Honda Civic",
+                ModelYear = 2021, DailyPrice= 400, Description = "Otomatik Vites" },
+            new Car { Id = 6, BrandId = 5, ColorId = 2, FuelTypeId = 4, Name = "Porche Taycan",
+                ModelYear = 2020, DailyPrice= 750, Description = "Otomatik Vites" },
+            new Car { Id = 7, BrandId = 1, ColorId = 4, FuelTypeId = 2, Name = "Ford Fiesta",
+                ModelYear = 2014, DailyPrice= 200, Description = "Manuel Vites" },
+            new Car { Id = 8, BrandId = 6, ColorId = 5, FuelTypeId = 3, Name = "Volkswagen Golf",
+                ModelYear = 2022, DailyPrice= 500, Description = "Otomatik Vites" },
+            new Car { Id = 8, BrandId = 6, ColorId = 5, FuelTypeId = 2, Name = "Volkswagen Polo",
+                ModelYear = 2017, DailyPrice= 340, Description = "Manuel Vites" }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+           Car DeleteCar = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(DeleteCar);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.SingleOrDefault(c => c.Id == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car updateCar = _cars.SingleOrDefault(c => c.Id == car.Id);
+            updateCar.Id = car.Id;
+            updateCar.BrandId = car.BrandId;
+            updateCar.ColorId = car.ColorId;
+            updateCar.FuelTypeId = car.FuelTypeId;
+            updateCar.Name = car.Name;
+            updateCar.ModelYear = car.ModelYear;
+            updateCar.Description = car.Description;
+            updateCar.DailyPrice = car.DailyPrice;
+        }
+    }
+}
